e567381f4f5c285704fe2b96899cdd279b0097a7
Jakub Kaczmarski <jkaczmar@c2s15d89.42wolfsburg.de>
2022-04-06 21:46:54 +0200

    make it look lake scary

--- main.c
+++ main.c
@@ -6,12 +6,29 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 19:12:30 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/04/06 21:39:22 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/04/06 21:46:36 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//Find one word in the string thingy
+int	get_to_actual_charac(int *starting_index, char *str)
+{
+	if(!str[*starting_index])
+	{
+		printf("NULL");
+		return 1;
+	}
+	while(str[*starting_index])
+	{
+		if(str[*starting_index] == ' ' || str[*starting_index] == '\t')
+			*starting_index = *starting_index + 1;
+		else
+			break;
+	}
+	return 0;
+}
 char *find_cmd(int *starting_index, char *str)
 {
 	//Find the command itself
@@ -21,27 +38,14 @@ char *find_cmd(int *starting_index, char *str)
 	first_size = *starting_index;
 	w_size = 0;
 	i = 0;
-	if(!str[*starting_index])
-	{
-		printf("NULL");
+	if(get_to_actual_charac(starting_index, str) == 1)
 		return NULL;
-	}
-	while(str[*starting_index])
-	{
-		if(str[*starting_index] == ' ' || str[*starting_index] == '\t')
-			*starting_index = *starting_index + 1;
-		else
-			break;
-	}
 	while(str[*starting_index])
 	{
 		if(str[*starting_index] != ' ' && str[*starting_index] != '\t')
-		{
 			w_size++;
-		}else
-		{
+		else
 			break;
-		}
 		*starting_index = *starting_index + 1;
 	}
 	if(w_size == 0)
@@ -64,15 +68,13 @@ int main(void)
 	int starting_index = 0;
 	char *wrd;
 	int i = 0;
+	char str[] = {"echo witam pozdrawiam z rodzinka"};
 	while(1)
 	{
-		wrd = find_cmd(&starting_index,"echo Sˇiemanko");
-		if(wrd)
-		{
-			printf("%d\n", i);
-		}
-		else
+		wrd = find_cmd(&starting_index,str);
+		if(!wrd)
 			break;
 		i++;
 	}
+	printf("%d", i);
 }
