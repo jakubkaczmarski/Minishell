4959b038ee6693320eb50369678306eea374a280
Jorit Tomala <jtomala@c2s12d59.42wolfsburg.de>
2022-04-07 14:46:04 +0200

    beginning of the replace feature

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:02:58 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/04/07 13:45:58 by jtomala          ###   ########.fr       */
+/*   Updated: 2022/04/07 14:45:42 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,9 @@ typedef struct s_data {
 //			parse
 //input.c
 void handle_input(t_data **info, char *input, int counter);
-void copy_envv(t_list **lst_envv, char **envv);
-void check_for_var(t_data *info, char *input);
-void replace_envv(t_data *info, char *input);
-
+void copy_envv(t_data **info, char **envv);
+void handle_input(t_data **info, char *input, int counter);
+void check_input(t_data *info, char *input);
+char *exchange_envv(t_data *info, char *str);
 
 #endif
--- parse/input.c
+++ parse/input.c
@@ -6,7 +6,7 @@
 /*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 08:11:12 by jtomala           #+#    #+#             */
-/*   Updated: 2022/04/07 13:45:37 by jtomala          ###   ########.fr       */
+/*   Updated: 2022/04/07 14:45:39 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,54 @@ Split command and arguments
 // 	if(current_index == 0)
 // 	printf("%s", info->envv->content);
 // }
+
+/*
+takes a variable(name) and gives its value back
+*/
+char *exchange_envv(t_data *info, char *str)
+{
+	char **split;
+	
+	split = NULL;
+	while (info->envv->next != NULL)
+	{
+		split = ft_split(info->envv->content, '=');
+		if (split[0] == str)
+			printf("REPLACE");
+		info->envv = info->envv->next;
+	}
+	return (split[1]);
+}
+
+/*
+checks the input for enviromental variables
+*/
+void check_input(t_data *info, char *input)
+{
+	char *var_start;
+	char **var;
+	
+	var_start = ft_strchr(input, '$');
+	var = NULL;
+	if (var_start != NULL)
+	{
+		var = ft_split(var_start, ' ');
+		printf("%s\n", var[0]);
+		exchange_envv(info, var[0]);
+	}
+	
+}
+
 /*
 put envv in a struct and replace them with the actual values
 */
 void handle_input(t_data **info, char *input, int counter)
 {
+	check_input(*info, input);
 	(*info)->cmd_table[counter] = malloc(sizeof(input));
 	(*info)->cmd_table[counter] = input;
-	// execute_cmd(*info, counter);	
+	printf("[%d]%s\n", counter, (*info)->cmd_table[counter]);
+	//execute_cmd(*info, counter);	
 }
 
 /*
@@ -36,23 +76,16 @@ handles the input given by the user and puts it into a struct
 */
 void copy_envv(t_data **info, char **envv)
 {
+	t_list *tmp;
 	int i;
 
 	i = 0;
-	while (input[i])
+	while (envv[i])
 	{
 		tmp = ft_lstnew(envv);
 		ft_lstadd_back(&info[0]->envv, tmp);
-		printf("%s\n", envv[i]);
+		//printf("%s\n", envv[i]);
 		i++;
 	}
 }
 
-/*
-if there is a variable in argv then the function searchs for this 
-in envv and replace it by the value
-*/
-void replace_envv(t_data *info, char *input)
-{
-	check_for_var(info, input);
-}
