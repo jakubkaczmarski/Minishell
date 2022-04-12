5f9d6a22f52f7c59390e17151685d35915383d96
Jorit Tomala <jtomala@c2s12d59.42wolfsburg.de>
2022-04-12 18:38:10 +0200

    problems

--- parse/input.c
+++ parse/input.c
@@ -6,7 +6,7 @@
 /*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 08:11:12 by jtomala           #+#    #+#             */
-/*   Updated: 2022/04/11 17:46:57 by jtomala          ###   ########.fr       */
+/*   Updated: 2022/04/12 18:37:53 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ char *exchange_envv(t_list *l_envv, char *str)
 	i = 0;
 	while (l_envv->next != NULL)
 	{
-		//printf("COMPARE: %s with %s\n", l_envv->content, str);
 		if (!ft_strncmp(l_envv->content, str, ft_strlen(str)))
 		{
 			value = ft_split(l_envv->content, '=');
@@ -48,36 +47,102 @@ char *exchange_envv(t_list *l_envv, char *str)
 /*
 checks the input for enviromental variables
 */
+// char *check_input(t_data *info, char *input)
+// {
+// 	char *var_start;
+// 	char **var;
+// 	char *value;
+// 	int i;
+	
+// 	var_start = ft_strchr(input, '$');
+// 	var = NULL;
+// 	value = malloc(sizeof(char *));
+// 	i = 1;
+// 	if (var_start)
+// 	{
+// 		var = ft_split(var_start, ' ');
+// 		value = exchange_envv(info->envv, var[0] + 1);
+// 		if (!value)
+// 			return (input);
+// 		input[ft_strchr(input, '$') - input] = '\0';
+// 		input = ft_strjoin(input, value);
+// 		input = ft_strjoin(input, " ");
+// 		while (var[i])
+// 		{
+// 			input = ft_strjoin(input, var[i++]);
+// 			input = ft_strjoin(input, " ");
+// 		}
+// 	}
+// 	return (input);
+// }
+
+char *get_value(char *input, int *counter)
+{
+	int i;
+
+	i = 0;
+	if (!input)
+		return (NULL);
+	while(input[i])
+	{
+		if (input[i] == ' ')
+			while (input[i])
+				input[i++] = '\0';
+		i++;
+	}
+	*counter = i + 1;
+	return (input);
+}
+
+char *modify_input(char *input, char *value, int counter, int len)
+{
+	int i;
+	int j;
+	char *safe_beg;
+	char *safe_end;
+
+	i = 0;
+	j = 0;
+	while (input[i])
+	{
+		if (input[i] == '$')
+		{
+			safe_beg = ft_substr(input, i, i);
+			safe_end = safe_beg;
+			printf("SAFE_BEG: %s%d\n", safe_beg, counter); //HERE IS THE PROBLEM
+			printf("SAFE_END: %s\n", safe_end);
+			while (j < len)
+				input[i++] = value[j++];
+		}
+		i++;
+	}
+	j = 0;
+	while (safe_end[j])
+		input[i++] = safe_end[j++];
+	input[i] = '\0';
+	return (input);
+}
+
 char *check_input(t_data *info, char *input)
 {
-	char *var_start;
-	char **var;
+	char *var;
 	char *value;
 	int i;
-	
-	var_start = ft_strchr(input, '$');
-	var = NULL;
+	int counter;
+
+	var = get_value(ft_strchr(input, '$'), &counter);
+	printf("INPUT: %s\n", input);
 	value = malloc(sizeof(char *));
+	if (!value)
+		return (input);
 	i = 1;
-	if (var_start)
+	if (var)
 	{
-		var = ft_split(var_start, ' ');
-		value = exchange_envv(info->envv, var[0] + 1);
-		if (!value)
-			return (input);
-		input[ft_strchr(input, '$') - input] = '\0';
-		input = ft_strjoin(input, value);
-		input = ft_strjoin(input, " ");
-		while (var[i])
-		{
-			input = ft_strjoin(input, var[i++]);
-			input = ft_strjoin(input, " ");
-		}
-		
-		//printf("END: %s\n", var_end);
+		value = exchange_envv(info->envv, var + 1);
+		input = modify_input(input, value, counter, ft_strlen(value));
 	}
+	free(value);
 	return (input);
-	
 }
 
 /*
