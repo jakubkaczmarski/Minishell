8de86cd9670b0b5cd5fb49c2e954d4f1b85f1cf8
Jakub Kaczmarski <jkaczmar@c2s15d89.42wolfsburg.de>
2022-04-24 14:46:10 +0200

    added correct parameter

--- build_ins/pwd.c
+++ build_ins/pwd.c
@@ -6,13 +6,13 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 11:27:00 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/04/24 12:58:15 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/04/24 14:45:57 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void pwd()
+void pwd(void)
 {
     char buff[4096];
     char *str = getcwd(&buff[0], sizeof(buff));
