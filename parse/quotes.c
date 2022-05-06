fadafd1602bddf73c7d16f8a1386fe9f2a559505
jtomala <jtomala@students.42wolfsburg.de>
2022-05-06 11:03:47 +0200

    norm

--- parse/quotes.c
+++ parse/quotes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quotes.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtomala <jtomala@students.42wolfsburg.de>  +#+  +:+       +#+        */
+/*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 13:57:02 by jtomala           #+#    #+#             */
-/*   Updated: 2022/04/26 06:13:16 by jtomala          ###   ########.fr       */
+/*   Updated: 2022/05/06 11:03:35 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@
 counts how many dollar signs are in the input 
 but keeps track on the quoations
 */
-char    *quote_handler(char *input)
+char	*quote_handler(char *input)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (input[i])
 	{
-		if (input[i] == 34) // && input[i - 1] == 34
+		if (input[i] == 34)
 			input = cut_off_douq(input);
 		i++;
 	}
@@ -34,27 +34,26 @@ char    *quote_handler(char *input)
 }
 
 /*
-should cut of the double quotes so I tested it with:
-    "'$USER'" -> '$USER' 
+cuts off the double quotes
 */
-char    *cut_off_douq(char *input)
+char	*cut_off_douq(char *input)
 {
-    int     i;
-    int     j;
-    int     index;
-    char    *new_input;
-    
-    i = 1;
-    j = 1;
-    index = 0;
-    new_input = ft_calloc(sizeof(char *), ft_strlen(input) - 2);
-    while (input[i] != 34)
-        i++;
-    index = ft_copy(new_input, input, i + 1); //copy up to the first
-    while (input[i + j] != 34)
-        j++;
-    index += ft_copy(&new_input[index], &input[i + 1], j); //copy until the second "
-    ft_copy(&new_input[index], &input[index + 2], 0);                  //copy after the secound "
-    free(input);
-    return (new_input);
+	int		i;
+	int		j;
+	int		index;
+	char	*new_input;
+
+	i = 1;
+	j = 1;
+	index = 0;
+	new_input = ft_calloc(sizeof(char *), ft_strlen(input) - 2);
+	while (input[i] != 34)
+		i++;
+	index = ft_copy(new_input, input, i + 1);
+	while (input[i + j] != 34)
+		j++;
+	index += ft_copy(&new_input[index], &input[i + 1], j);
+	ft_copy(&new_input[index], &input[index + 2], 0);
+	free(input);
+	return (new_input);
 }
