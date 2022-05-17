fbcb582a3fddaa3adeb88c8f1b144cc43fe2d9e7
jtomala <jtomala@students.42wolfsburg.de>
2022-05-17 19:53:55 +0200

    fix

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -3,22 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/30 10:54:48 by jtomala           #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2022/05/16 17:08:21 by jtomala          ###   ########.fr       */
-=======
-/*   Updated: 2022/05/17 17:07:00 by jtomala          ###   ########.fr       */
->>>>>>> jorit
-=======
-/*   Updated: 2022/05/17 19:24:36 by jtomala          ###   ########.fr       */
->>>>>>> jorit
-=======
-/*   Updated: 2022/05/17 18:25:08 by jkaczmar         ###   ########.fr       */
->>>>>>> 39d4b09b2fcbda014c5e3088c978c70948a23fbb
+/*   Created: 2022/05/17 19:53:47 by jtomala           #+#    #+#             */
+/*   Updated: 2022/05/17 19:53:50 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
