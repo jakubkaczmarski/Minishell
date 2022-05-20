f2e71ce80df81555c45af503a2b09c4c509ab934
jtomala <jtomala@students.42wolfsburg.de>
2022-05-20 13:25:45 +0200

    new struct

--- parse/struct.c
+++ parse/struct.c
@@ -6,7 +6,7 @@
 /*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 13:59:32 by jtomala           #+#    #+#             */
-/*   Updated: 2022/05/20 08:50:59 by jtomala          ###   ########.fr       */
+/*   Updated: 2022/05/20 13:25:21 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,151 +24,108 @@ char *insert_space(char *string, int i)
 	new_string = malloc(sizeof(char *) * ft_strlen(string) + 1);
 	if (!new_string)
 		return (string);
-	ft_copy(new_string, string, i + 1);
-	new_string[i] = ' ';
-	ft_copy(&new_string[i + 1], &string[i], 0);
+	ft_copy(new_string, string, i + 2);
+	new_string[i + 1] = ' ';
+	ft_copy(&new_string[i + 2], &string[i + 1], 0);
 	return (new_string);
 }
 
-/*
-<something.txt
-< something.txt
-<<something.txt
-go though the string and the first word after the redirect is the filename
-for each redirect you have to have one index in the array
-if there is <sth then insert a space: < sth
-*/
-int redirect_input(t_cmd *cmd, char *string)
-{
-	int	i;
-	int	j;
-	int	start;
-	int	index;
-
-	i = 0;
-	j = 0;
-	start = 0;
-	index = 0;
-	while (string[i])
-	{
-		if (string[i + j] == '<')
-		{
-			start = i;
-			if (string[i++ + (++j)] != ' ' && string[i] != '<')
-			 	string = insert_space(string, i);
-			while (string[i + j] != ' ')
-				j++;
-			cmd->in[index] = ft_substr(string, start, j + 1);
-			printf("in_redirect[%d]: %s\n", index, cmd->in[index]);
-			index++;
-		}
-		i += j;
-		j = 0;
-		i++;
-	}
-	return (i);
-}
-
-/*
-output
-*/
-int redirect_output(t_cmd *cmd, char *string)
-{
-	int	i;
-	int	j;
-	int	start;
-	int	index;
-
-	i = 0;
-	j = 0;
-	start = 0;
-	index = 0;
-	while (string[i])
-	{
-		if (string[i + j] == '>')
-		{
-			start = i;
-			if (string[i++ + j] != ' ' && string[i + 1] != '>')
-			 	string = insert_space(string, i++);
-			while (string[i + j++] != '\0' && string[i + j] != '>')
-				j++;
-			cmd->out[index] = ft_substr(string, start, j + 1);
-			printf("out_redirect[%d]: %s\n", index, cmd->out[index]);
-			index++;
-			j--;
-		}
-		i += j;
-		j = 0;
-		i++;
-	}
-	return (j);
-}
-
-/*
-cmd
-*/
-int redirect_cmd(t_cmd *cmd, char *string)
+void track_redirection(t_cmd *cmd, char *string, char c, int j)
 {
 	int i;
-	int position;
-	char *tmp;
 
 	i = 0;
-	position = 0;
-	while (cmd->in[i])
-		position += ft_strlen(cmd->in[i++]);
-	i = 0;
-	while (string[position + i] != '>')
+	if (string[i + 1] == ' ')
+		i = 2;
+	while (string[i] != ' ' && string[i] != '\0')
 		i++;
-	tmp = ft_substr(string, position, i);
-	cmd->cmd = ft_split(tmp, ' ');
-	i = 0;
-	while (cmd->cmd[i])
+	if (c == '<')
 	{
-		printf("cmd[%d]: %s\n", i, cmd->cmd[i]);
+		cmd->in[j] = ft_substr(string, 0, i);
+		printf("in_red[%d]: %s\n", j, cmd->in[j]);
+	}
+	else if (c == '>')
+	{
+		cmd->out[j] = ft_substr(string, 0, i);
+		printf("out_red[%d]: %s\n", j, cmd->out[j]);
+	}
+}
+
+void redirections(t_cmd *cmd, char *string)
+{
+	int i;
+	int in_j;
+	int out_j;
+
+	i = 0;
+	in_j = 0;
+	out_j = 0;
+	while (string[i])
+	{
+		if (string[i] == '<')
+		{
+			if (string[i + 1] != ' ')
+				string = insert_space(string, i);
+			track_redirection(cmd, &string[i], '<', in_j++);
+		}
+		else if (string[i] == '>')
+		{
+			if (string[i + 1] != ' ')
+				string = insert_space(string, i);
+			track_redirection(cmd, &string[i], '>', out_j++);
+		}
 		i++;
 	}
-	return (i);
 }
+
+int cmd_tracker(t_cmd *cmd, char *string)
+{
+	int i;
+	int j;
+	int len;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(string);
+	while (string[i] != ' ')
+	{
+		if (string[i + 2] == '<' || string[i + 2] == '>')
+			i++;
+		i++;
+	}
+	while (string[i + j] != '<' && string[i + j] != '>' \
+		&& string[i + j] != '\0' && (i + j) != len)
+		j++;
+	cmd->cmd = ft_split(ft_substr(string, i, j), ' ');
+	i = 0;
+	while(cmd->cmd[i])
+	{
+		printf("cmd: %s\n", cmd->cmd[i++]);
+	}
+	return (1);
+}
+
 
 void handle_struct(t_data *info)
 {
 	int i;
 	int j;
 	int position;
+	t_cmd *cmd;
 
 	i = 0;
 	j = 0;
 	position = 0;
-	info->cmd = ft_calloc(sizeof(t_cmd *), 1);
-	info->cmd->in = ft_calloc(sizeof(char **), 1);
-	info->cmd->in[0] = ft_calloc(sizeof(char *), 1);
-	info->cmd->in[0][0] = '\t';
-	info->cmd->out = ft_calloc(sizeof(char **), 1);
-	info->cmd->out[0] = ft_calloc(sizeof(char *), 1);
-	info->cmd->out[0][0] = '\t';
-	info->cmd->cmd = ft_calloc(sizeof(char **),1 );
 	while (info->cmd_table[i])
 	{
-		if (ft_strchr(info->cmd_table[i], '<') == NULL && ft_strchr(info->cmd_table[i], '>') == NULL)
-		{
-			while(info->cmd_table[i])
-			{
-				info->cmd->cmd[i] = info->cmd_table[i];
-				printf("CMd : %s\n", info->cmd->cmd[i]);
-				i++;
-			}
-			info->amount_cmd = i;
-			printf("Command ammout %d\n", info->amount_cmd);
-			break;
-		}
 		printf("------ROUND %d--------\n", i);
-		info->cmd->in[i] = ft_calloc(sizeof(char *) , 1);
-		info->cmd->out[i] = ft_calloc(sizeof(char *) , 1);
-		position += redirect_input(&(info->cmd[i]), info->cmd_table[i]);
-		redirect_cmd(&(info->cmd[i]), info->cmd_table[i]);
-		redirect_output(&(info->cmd[i]), info->cmd_table[i]);
-		
+		info->cmd[i] = *(t_cmd*)ft_calloc(sizeof(t_cmd *), 1);
+		cmd = &(info->cmd[i]);
+		cmd->in = ft_calloc(sizeof(char **), 1);
+		cmd->out = ft_calloc(sizeof(char **), 1);
+		redirections(&(info->cmd[i]), info->cmd_table[i]);
+		info->amount_cmd += cmd_tracker(&(info->cmd[i]), info->cmd_table[i]);
+		printf("amount_cmd: %d\n", info->amount_cmd);	
 		i++;
 	}
 }
