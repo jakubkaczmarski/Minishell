271b22ae461d5406da6f56c80baf26d951c1893f
= <kuba.kaczmarski312@gmail.com>
2022-06-07 00:50:41 +0200

    fix

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 13:07:31 by jtomala           #+#    #+#             */
-/*   Updated: 2022/06/06 21:28:23 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/06/07 00:50:13 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,7 @@ void	ft_echo(t_data *info)
 		i++;
 	}
 	if (!info->cmd[info->index].cmd[1] && !info->cmd[info->index].cmd[2])
-	{
 		return ;
-	}
 	while (info->cmd[info->index].cmd[i])
 		printf("%s ", info->cmd[info->index].cmd[i++]);
 	if (flag == 0)
